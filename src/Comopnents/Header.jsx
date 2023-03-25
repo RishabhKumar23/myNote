e57752a335cc0ea4162f2181c7ab670ff55ba42f
Rishabh Kumar <112078029+RishabhKumar23@users.noreply.github.com>
2023-03-25 15:57:17 +0530

    Add files via upload

---
+++ src/Comopnents/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import NoteIcon from '@mui/icons-material/Note';
+function Header(){
+    return <header>
+        <h1><NoteIcon />MY-Note</h1>
+    </header>
+}
+
+export default Header;
